171dbc8bb6b6b99f4d2e42b73bdf6c209160e4f3
thereactsamurai <katanacoding@hotmail.com>
2022-07-26 11:53:46 +0500

    hero & about fixes final

--- components/HomePage/About.js
+++ components/HomePage/About.js
@@ -3,7 +3,7 @@ import AboutImg from "../../public/images/about.jpg";
 
 export const About = () => {
   return (
-    <section className="container container-width mt-8 grid min-h-[75vh] text-white lg:mt-16 lg:grid-cols-2">
+    <section className="container container-width grid min-h-[75vh] text-white lg:mt-16 lg:grid-cols-2">
       <div className="relative w-full h-[50vh] row-start-2 lg:row-start-1">
         <Image
           className="object-cover"
@@ -15,12 +15,12 @@ export const About = () => {
           priority
         />
       </div>
-      <section className="pt-6 pb-12 flex flex-col justify-center lg:px-12 lg:py-0">
+      <section className="pt-2 pb-12 flex flex-col justify-center lg:px-12 lg:py-0">
         <h2 className="text-4xl font-bold text-center mb:text-5xl lg:text-left">
           We lead this industry
         </h2>
 
-        <p className="mt-8 mb-12 mx-3 max-w-2xl lg:mx-0 lg:mb-16">
+        <p className="break-all mt-4 mb-12 mx-4 max-w-2xl text-zinc-400 lg:mx-0 lg:mb-16">
           Integer eros lectus, efficitur at rhoncus eu, faucibus vitae velit.
           Integer vehicula volutpat sagittis. Pellentesque tempus diam vitae
           hendrerit vestibulum. Curabitur ac nulla nec lacus laoreet fringilla
--- components/HomePage/Hero.js
+++ components/HomePage/Hero.js
@@ -7,24 +7,26 @@ import { BsArrowRight } from "react-icons/bs";
 export const Hero = () => {
   return (
     <>
-      <section className="container container-width pt-24 min-h-screen flex flex-col justify-start items-center text-white">
-        <h2 className="mb-2 uppercase font-bold tracking-widest text-zinc-400 mb:text-lg lg:text-2xl">
+      <section className="container container-width pt-24 min-h-screen flex flex-col justify-start items-start text-white">
+        <h2 className="ml-3 lg:ml-0 mb-2 uppercase font-bold tracking-widest text-zinc-400 mb:text-lg lg:text-2xl">
           Discover. Live. Relish.
         </h2>
-        <h1 className="uppercase font-bold text-4xl mb:text-5xl lg:text-7xl">
+        <h1 className="ml-3 lg:ml-0 uppercase font-bold text-4xl mb:text-5xl lg:text-7xl">
           Elegance is the <span className="text-brand-accent">new modern.</span>{" "}
         </h1>
 
-        <p className="mt-4 mb-2 mx-2 text-zinc-500 lg:mx-0 lg:text-lg lg:my-4">
+        <p className="ml-3 lg:ml-0 mt-4 mb-2 text-zinc-500 lg:mx-0 lg:text-lg lg:my-4">
           Together we can craft and build things that last because{" "}
           <span className="text-zinc-400">
             we believe in great architecture.
           </span>
         </p>
+        <div className="ml-3 lg:ml-0">
+          <Button href="/properties">
+            View properties <BsArrowRight className="text-2xl" />
+          </Button>
+        </div>
 
-        <Button href="/properties">
-          View properties <BsArrowRight className="text-2xl" />
-        </Button>
         <div className="mt-10 relative w-full h-[50vh] lg:h-[75vh] lg:mt-12 ">
           <Image
             src={HeroImg}
